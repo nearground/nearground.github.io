cab569b65696249c9f7e74670ac4ff8ec609446b
Nearground <54041291+Nearground@users.noreply.github.com>
2025-07-29 08:44:31 +0000

    Deploying to gh-pages from @ Nearground/Nearground.github.io@5b6ae86ed382b57624af508524278c05a526d06e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -471,7 +471,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-open-for-work",
+          title: 'Open for work',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2025-07-25-open-for-work/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
